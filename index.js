7cdd561cbfafa1ac652770815b71f3f85e3ed27d
sharanabasappadhammur <108877043+sharanabasappadhammur@users.noreply.github.com>
2023-10-09 18:15:39 +0530

    3rd

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// import axios from "axios";
-// import tough from "tough-cookie"
-// import fetch from "node-fetch";
-// import express, { response } from "express"
-// // const cron = require('node-cron');
-// import cron from 'node-cron'
-// const app = express()
-// import dotenv from 'dotenv'
-// dotenv.config()
-// import cors from 'cors'
-// app.use(cors())
-// app.use(express.json())
+import axios from "axios";
+import tough from "tough-cookie"
+import fetch from "node-fetch";
+import express, { response } from "express"
+// const cron = require('node-cron');
+import cron from 'node-cron'
+const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
+import cors from 'cors'
+app.use(cors())
+app.use(express.json())
 
-// app.get("/",(req,res)=>{
-//     res.send("wqwwq")
-// })
+app.get("/",(req,res)=>{
+    res.send("wqwwq")
+})
 
 
-// app.listen(process.env.PORT, async () => {
-//     try {
-//         console.log("connected to server")
-//     }
-//     catch (error) {
-//         console.log(error.message)
-//     }
-// })
+app.listen(process.env.PORT, async () => {
+    try {
+        console.log("connected to server")
+    }
+    catch (error) {
+        console.log(error.message)
+    }
+})
 
 
